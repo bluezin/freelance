aafef032edfbead56719e8ebee35bad8b49bbafb
yadira <yadiraco304@gmail.com>
2026-09-23 11:53:15 -0500

    feat: add routes

--- next.config.ts
+++ next.config.ts
@@ -18,9 +18,25 @@ const nextConfig: NextConfig = {
       },
       {
         basePath: false,
+        source: "/restaurant",
+        destination: "https://restaurant-delta-blond-37.vercel.app/restaurant",
+      },
+      {
+        basePath: false,
         source: "/restaurant/:path*",
         destination:
-          "https://restaurant-delta-blond-37.vercel.app/restaurant:path*",
+          "https://restaurant-delta-blond-37.vercel.app/restaurant/:path*",
+      },
+      {
+        basePath: false,
+        source: "/inmoviliaria",
+        destination: "https://inmoviliaria-mauve.vercel.app/inmoviliaria",
+      },
+      {
+        basePath: false,
+        source: "/inmoviliaria/:path*",
+        destination:
+          "https://inmoviliaria-mauve.vercel.app/inmoviliaria/:path*",
       },
     ];
   },
