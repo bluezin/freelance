82a0a4a98791061a35432952371aa6147ed1cbea
yadira <yadiraco304@gmail.com>
2026-09-23 10:21:27 -0500

    feat: add rewrite

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites() {
+    return [
+      {
+        basePath: false,
+        source: "/estudio-abogados/:path*",
+        destination:
+          "https://estudio-de-abogados-eosin.vercel.app/estudio-abogados/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
